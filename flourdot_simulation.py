c2a7c4be4252a47308f04fbafbe853470becf8cf
Bruce Wallin <bruce.wallin@colorado.edu>
2014-10-21 17:44:33 -0600

    Sim outputs true model.

--- flourdot_simulation.py
+++ flourdot_simulation.py
@@ -14,27 +14,34 @@ def generate_image(locus_shape, num_dots):
     p,q,r = psf_template.shape
     depths = randint(0,r, num_dots)
     locs = [(randint(0, locus_shape[0]), randint(0, locus_shape[1])) for _ in xrange(num_dots)]
-    intensity = randint(50, 150, num_dots)
+    intensities = randint(50, 150, num_dots)
 
     image_shape = array((p,q))+array(locus_shape)
     image = zeros(image_shape)
     for l in xrange(num_dots):
         i,j = locs[l]
         k = depths[l]
-        image[i:i+p, j:j+q] += intensity[l]*psf_template[:,:,k]
+        image[i:i+p, j:j+q] += intensities[l]*psf_template[:,:,k]
 
-    return image
+    return depths, locs, intensities, image
 
 
 @click.command()
 @click.option('--size', '-s', default=5, help='Width of target window in pixels')
 @click.option('--num-dots', '-n', default=3, help='Number of dots in target window')
-@click.option('--output-filename', '-o', default='localization_test_image.tif', help='Name of output tif')
+@click.option('--name', '-o', default='localization_test_image.tif', help='Name of output')
 @click.option('--plot', '-p', is_flag=True, help='Show plot')
-def main(size, num_dots, output_filename, plot):
+def main(size, num_dots, name, plot):
     locus_shape = (size, size)
-    image = generate_image(locus_shape, num_dots)
-    tifffile.imsave(output_filename, image)
+    depths, locs, intensities, image = generate_image(locus_shape, num_dots)
+    tifffile.imsave(name+'.tif', image)
+    cPickle.dump({
+            'depths': depths,
+            'locs': locs,
+            'intensities': intensities
+            },
+            open(name+'.pkl', 'w')
+            )
     if plot:
         from pylab import pcolormesh, show
         pcolormesh(image)
